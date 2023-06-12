2ca0f046a5b5327139a47ea7506f846bf8e420e1
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-12 14:14:54 +0530

    Create array_methods.sv

---
+++ array_methods.sv
@@ -0,0 +1,12 @@
+module array();
+  int count, total, n[] = {1,4,6,15,8,3,10,05,16,12};
+  
+  count = n.sum with ((item > 7));
+  total = n.sum with ((item < 7));
+  
+  n.reverse();
+  n.sort();
+  n.shuffle();
+  n.rsort();
+  
+endmodule
