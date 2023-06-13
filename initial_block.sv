b5be7c6465b028766f648b6eb041bd70536e616e
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-14 00:26:39 +0530

    Create initial_block.sv

---
+++ initial_block.sv
@@ -0,0 +1,11 @@
+module example();                                                   
+    reg clock, reset, enable, data;                           
+
+    initial                                                    
+        begin
+             clock = 0;                                     
+             reset = 0;
+             enable = 0;
+             data = 0;
+        end
+endmodule
