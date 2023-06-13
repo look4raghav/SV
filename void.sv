673f1dc04548e299dcada9c20a203593847916b1
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-14 00:13:11 +0530

    Create void.sv

---
+++ void.sv
@@ -0,0 +1,8 @@
+function void fxn(int var1, var2);
+  $display(var1, var2);
+endfunction: fxn
+
+function int product(input int var1, var2, output var3);
+    product = var1 * var2;
+    var3 = product*2;
+endfunction
