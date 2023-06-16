f45e2d513ee318330500e4e3ae7f2161cc7d3612
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-16 17:50:29 +0530

    Update super.sv

--- Classes/super.sv
+++ Classes/super.sv
@@ -14,7 +14,7 @@ class extPacket extends Packet;
     $display("[Child]addr = 0x%0h", addr);
   endfunction
   
-  function.new();
+  function new();
     super.new();
   endfunction
 endclass
@@ -31,3 +31,11 @@ module tb;
       ep.display();
     end
 endmodule
+
+
+
+/*EXECUTION ERRROR..         Exit code expected: 0, received: 1
+Error in execution: [No such image: 1686917615048-0 (tag: latest)
+]. Your run may have reached the memory limit of 500MB.
+Done
+*/
