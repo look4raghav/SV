77de3db1c8072fbb65f479d1993067c9988ff804
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-16 16:45:01 +0530

    Create class.sv

---
+++ Classes/class.sv
@@ -0,0 +1,10 @@
+Class training_c;
+int coutse_fee;
+function int total_subjects;
+  return course_fee;
+endfunction
+
+task deposit(input int tax);
+  course_fee = course_fee + tax;
+endtask
+endclass
