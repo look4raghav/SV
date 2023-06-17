fcf8465bf021aecd97f77d76c654a54bfdd14e4c
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-17 22:50:33 +0530

    Update random_assert.sv

--- Classes/random_assert.sv
+++ Classes/random_assert.sv
@@ -2,7 +2,7 @@ class myPacket;
   rand bit[1:0] mode;
   randc bit[2:0] key;
   constraint c_mode1{mode<3;}
-  constraint c_key1{key>2; key<7}
+  constraint c_key1{key>2; key<7;}
   
   function display();
     $display("MODE: 0x%0h Key: 0x%0h", mode, key);
