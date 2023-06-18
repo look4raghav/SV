874350ae42cccbc4a35d2cb25fb3e87a86e84c51
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-19 01:23:06 +0530

    Create constraint_overriding.sv

---
+++ Classes/constraint_overriding.sv
@@ -0,0 +1,14 @@
+class transaction;
+  rand bit[15:0] pkt_len;
+  constraint oversize  {pkt_len >= 16'hff00;}
+endclass: transaction
+class trans_child extends transaction;
+  constraint oversize{pkt_len <= 16'hff50;}
+endclass: trans_child
+trans_child trans = new;
+initial
+  begin
+    int success;
+    success = trans.randomize();
+    $display("Pkt len = %h", trans.pkt_len);
+  end
