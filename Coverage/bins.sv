2760431d1d0ddcfc07263deb14e66b86521c73ed
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-27 14:56:11 +0530

    Create bins.sv

---
+++ Coverage/bins.sv
@@ -0,0 +1,22 @@
+module tb;
+  bit[2:0] mode;
+
+  covergroup cg;
+    coverpoint mode
+    {
+      bins one = {1};
+      bine five = {5};
+    }
+  endgroup
+
+  initial
+    begin
+      cg cg_inst = new();
+      for (int i=0; i<5; i++) begin
+        #10 mode=$random;
+        $display ("[%0t] mode=0x%0h", $time, mode);
+        cg_inst.sample();
+      end
+      $display("Coverage = %0.2f %%", cg_inst.get_inst_coverage());
+    end
+endmodule
