75622d37a651936e4ba59f0b9ed2e1cd7de83778
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-27 11:07:19 +0530

    Create covergroup.sv

---
+++ Coverage/covergroup.sv
@@ -0,0 +1,27 @@
+module tb;
+  bit[1:0] mode;
+  bit[2:0] cfg;
+
+  bit clk;
+  always #20 clk = ~clk;
+
+  covergroup cg@(posedge clk);
+    coverpoint mode;
+  endgroup
+
+  cg cg_inst;
+  cg_inst = new();
+
+  for(int i=0; i<5; i++) begin
+    @(negedge clk);
+    mode = $random;
+    cfg = $random;
+    $display ("[%0t] mode=0x%0h", $time, mode, cfg);
+  end
+  end
+
+  initial begin
+    #500 $display("COVERAGE = %0.2f %%", cg_inst.get_inst_coverage());
+    $finish;
+  end
+endmodule
